f05c9e50f5ccd1d081577d20cf7f8fe2281c515c
Sonu <sonustar.in@gmail.com>
2022-08-25 20:20:18 +0530

    Add project files.

---
+++ SinghPlayer/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SinghPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpenFiles = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFromSec = new System.Windows.Forms.TextBox();
+            this.txtTakeSec = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tBarProcessing = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.tBarProcessing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFiles
+            // 
+            this.btnOpenFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFiles.Name = "btnOpenFiles";
+            this.btnOpenFiles.Size = new System.Drawing.Size(74, 23);
+            this.btnOpenFiles.TabIndex = 1;
+            this.btnOpenFiles.Text = "Open Files";
+            this.btnOpenFiles.UseVisualStyleBackColor = true;
+            this.btnOpenFiles.Click += new System.EventHandler(this.btnOpenFiles_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(321, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // txtFromSec
+            // 
+            this.txtFromSec.Location = new System.Drawing.Point(173, 14);
+            this.txtFromSec.Name = "txtFromSec";
+            this.txtFromSec.Size = new System.Drawing.Size(36, 20);
+            this.txtFromSec.TabIndex = 3;
+            this.txtFromSec.Text = "60";
+            // 
+            // txtTakeSec
+            // 
+            this.txtTakeSec.Location = new System.Drawing.Point(296, 14);
+            this.txtTakeSec.Name = "txtTakeSec";
+            this.txtTakeSec.Size = new System.Drawing.Size(37, 20);
+            this.txtTakeSec.TabIndex = 4;
+            this.txtTakeSec.Text = "60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "From Seconds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Take Second";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(12, 276);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 13);
+            this.lblTime.TabIndex = 7;
+            // 
+            // tBarProcessing
+            // 
+            this.tBarProcessing.Enabled = false;
+            this.tBarProcessing.Location = new System.Drawing.Point(92, 272);
+            this.tBarProcessing.Name = "tBarProcessing";
+            this.tBarProcessing.Size = new System.Drawing.Size(239, 45);
+            this.tBarProcessing.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 308);
+            this.Controls.Add(this.tBarProcessing);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTakeSec);
+            this.Controls.Add(this.txtFromSec);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnOpenFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Singh Player";
+            ((System.ComponentModel.ISupportInitialize)(this.tBarProcessing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpenFiles;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFromSec;
+        private System.Windows.Forms.TextBox txtTakeSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar tBarProcessing;
+    }
+}
---
+++ SinghPlayer/Form1.cs
@@ -0,0 +1,133 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinghPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] files, path;
+        int index = 0;
+        private void btnOpenFiles_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                files = openFileDialog1.SafeFileNames;
+
+                path = openFileDialog1.FileNames;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBox1.Items.Add(files[i]);
+                }
+
+                Play(path[index]);//start  
+            }
+        }
+
+ 
+
+        WaveOutEvent player;
+        public void Play(string fileurl)
+        {
+            try
+            {
+                player = new WaveOutEvent();
+                var file = new AudioFileReader(fileurl);
+
+                var trimmed = new OffsetSampleProvider(file);
+                double fadeMilisec = 0;//take milisecoinds for faiding
+                long mediaLengthSeconds = 0;
+
+                trimmed.SkipOver = TimeSpan.FromSeconds(Convert.ToDouble(txtFromSec.Text));
+                trimmed.Take = TimeSpan.FromSeconds(Convert.ToDouble(txtTakeSec.Text));
+                fadeMilisec = (Convert.ToDouble(txtTakeSec.Text) * 1000) - 1300;
+                mediaLengthSeconds = long.Parse(txtTakeSec.Text);
+
+                player.PlaybackStopped += new EventHandler<StoppedEventArgs>(audioOutput_PlaybackStopped);
+
+                var fadeOut = new DelayFadeOutSampleProvider(trimmed);//
+                fadeOut.BeginFadeOut(fadeMilisec, 1300);//
+
+                player.Init(fadeOut);
+
+                //player.Init(trimmed); 
+                player.Play();
+
+                listBox1.SelectedIndex = index;
+                listBox1.Refresh();
+
+                //call timer
+                timer1.Tick += Tick;
+                CountDown(mediaLengthSeconds);
+                //call timer end
+            }
+            catch (Exception ex)
+            {
+                if (index < listBox1.Items.Count)
+                {
+                    Play(path[index]);
+                }
+            }
+        }
+
+        //timer job begin
+        DateTime start; //use for timer
+        long s; //use for timer 
+
+        public void CountDown(long seconds)
+        {
+            tBarProcessing.Minimum = 0;
+            tBarProcessing.Maximum = Convert.ToInt32(seconds);
+
+            start = DateTime.Now;
+            s = seconds;
+            timer1.Start();
+        }
+        private void Tick(object sender, EventArgs e)
+        {
+            Int32 count = 0;
+            var remainingSeconds = s - (DateTime.Now - start).TotalSeconds;
+            count = Convert.ToInt32((s - remainingSeconds));
+            lblTime.Text = string.Format("{0}", Convert.ToInt32(count));
+            tBarProcessing.Value = count;
+            if (count == s)
+            {
+                timer1.Stop();
+                lblTime.Text = "Done!";
+                return;
+            }
+
+
+        }
+
+        private void audioOutput_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            index = index + 1;
+            if (index < listBox1.Items.Count)
+            {
+                //player.Stop();
+                //player.Dispose();
+
+                Play(path[index]);
+            }
+        }
+
+
+
+
+    }
+}
